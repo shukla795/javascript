adf35795637c25cccf5db64b133c47773251cd6a
shukla795 <61756525+shukla795@users.noreply.github.com>
2020-08-22 13:22:33 +0530

    Add files via upload

---
+++ calculator.js
@@ -0,0 +1,111 @@
+window.addEventListener('DOMContentLoaded', bindEvents);
+
+function bindEvents(){
+    // document.querySelector('#b1'); // reading a button by # id
+    // addEventListener --> attaching event
+    // event name and function name to be call
+    document.querySelector('#input').addEventListener('click',getNumber);
+    document.querySelector('#b2').addEventListener('click',clearScreen);
+    document.querySelector('#input').addEventListener('click',getOperand);
+    document.querySelector('#input').addEventListener('click',backspace);
+    //document.querySelector('#lname').addEventListener('keyup',executeGreet);
+}
+
+function getNumber(num) {
+    var input_var = document.getElementById('input');
+
+   switch(num){
+         case 1:
+         input_var.value += '1';
+         //document.getElementById('input').innerHTML=input_var;
+         break;
+         case 2:
+         input_var.value += '2';
+         break;
+         case 3:
+         input_var.value += '3';
+         break;
+         case 4:
+         input_var.value += '4';
+         break;
+         case 5:
+         input_var.value += '5';
+         break;
+         case 6:
+         input_var.value += '6';
+         break;
+         case 7:
+         input_var.value += '7';
+         break;
+         case 8:
+         input_var.value += '8';
+         break;
+         case 9:
+         input_var.value += '9';
+         break;
+         case 0:
+         input_var.value += '0';
+         break;   
+   }
+}
+
+function clearScreen(){
+    document.getElementById('input').value="";
+    document.getElementById('answer').value="";
+}
+
+function getOperand(operand){
+    var input_var = document.getElementById('input');
+    
+    switch(operand){
+      
+       case '+':
+        input_var.value += '+';
+        break;
+        case '-':
+        input_var.value += '-';
+        break;
+        case '*':
+        input_var.value += '*';
+        break;
+        case '/':
+        input_var.value += '/';
+        break;
+        case '+/-':
+        input_var.value += '-' +input_var.value ;
+        break;
+    }
+}
+
+
+function backspace(){
+    var input_var = document.getElementById('input');
+    var x= input_var.value;
+      if(x.length>0){
+
+     x = x.substring(0,x.length-1);
+        input_var.value = x;
+      }
+}
+
+
+ function compute(){
+    var input_var = document.getElementById('input');
+    ans = Math.floor(+eval(input_var.value));
+    document.getElementById('answer').value=ans;
+ }
+
+
+  var i=0;
+     function brackets(){
+        var input_var = document.getElementById('input');
+        if(i==0){
+           input_var.value = '(';
+          i =1;
+        }else if(i==1){
+    
+            input_var.value = ')';
+            i =0;
+
+        }
+     }
